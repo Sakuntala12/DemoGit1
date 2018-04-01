a7143a0ceba8209b120ac0ba760453ab49b51219
Sakuntala12 <37964663+Sakuntala12@users.noreply.github.com>
2018-04-01 04:36:19 -0700

    sakuntala123

--- Git_Demo/src/DuplicateString.java
+++ Git_Demo/src/DuplicateString.java
@@ -5,7 +5,7 @@ public class DuplicateString {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String [] str={"Nilava123","Sakuntala","Nilava","Sakuntala","Sakuntala"};
+		String [] str={"Nilava123","Sakuntala","Nilava","Sakuntala123","Sakuntala"};
 		for(int i=0;i<=str.length-1;i++){
 				for(int j=i+1;j<=str.length-1;j++){
 						if(str[i].equals(str[j])){
