255dcca70258a2aa74e4e55f75c1d4504778cc05
sukundi <sukundi@sukundi-PC>
2018-04-01 04:05:20 -0700

    changed duplicate string code

---
+++ Git_Demo/src/DuplicateString.java
@@ -0,0 +1,21 @@
+
+public class DuplicateString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String [] str={"Nilava123","Sakuntala","Nilava","Sakuntala","Sakuntala"};
+		for(int i=0;i<=str.length-1;i++){
+				for(int j=i+1;j<=str.length-1;j++){
+						if(str[i].equals(str[j])){
+							System.out.println("Duplicate Value=======>"+str[j]);
+						}
+				}
+		}
+
+	}
+
+}
+
+
